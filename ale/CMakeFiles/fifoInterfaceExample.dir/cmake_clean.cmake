e3077c87d7273f00343eb0dbb443355172e743d0
mayifei35 <mayifei35@gmail.com>
2016-09-30 10:40:23 -0600

    new version of ale

---
+++ ale/CMakeFiles/fifoInterfaceExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/fifoInterfaceExample.dir/doc/examples/fifoInterfaceExample.cpp.o"
+  "doc/examples/ale-fifoInterfaceExample.pdb"
+  "doc/examples/ale-fifoInterfaceExample"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/fifoInterfaceExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ ale/CMakeFiles/videoRecordingExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/videoRecordingExample.dir/doc/examples/videoRecordingExample.cpp.o"
+  "doc/examples/ale-videoRecordingExample.pdb"
+  "doc/examples/ale-videoRecordingExample"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/videoRecordingExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
